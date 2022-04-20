8295ea3e079872de174621670a5f5270f3390fb1
Abdul Hakkeem P A <abdulhakkeempa2002@gmail.com>
2022-04-20 17:26:59 +0530

    Update numberPattern_3.cpp

--- numberPattern_3.cpp
+++ numberPattern_3.cpp
@@ -1,5 +1,5 @@
-//		  1
-//	    1 2 1
+//	  1
+//	1 2 1
 //    1 2 3 2 1
 //  1 2 3 4 3 2 1
 
@@ -9,16 +9,21 @@ using namespace std;
 int main() {
 	int k;
 	int rows =7;
+	#row wise iteration
 	for (int i = 1; i <= rows; ++i) {
+		#for spacing
 		for (int j = 0; j < (rows-i); ++j) {
 			cout<<"  ";
 		}
+		#for printing the pattern upto the max value
 		for (k = 1; k <= i; ++k) {
 			cout<<k<<" ";
 		}
+		#printing the balance pattern from the max to 1.
 		for (int l = (k-2);l>0 ; l--) {
 			cout<<l<<" ";
 		}
+		#next row.
 		cout<<endl;
 	}
 	return 0;
